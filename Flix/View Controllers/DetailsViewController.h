ff9cdcc76975186706b42263af1e9791e55e5465
Claudia Haddad <cmhaddad@fb.com>
2018-06-28 13:47:05 -0700

    done w required

---
+++ Flix/View Controllers/DetailsViewController.h
@@ -0,0 +1,15 @@
+//
+//  DetailsViewController.h
+//  Flix
+//
+//  Created by Claudia Haddad on 6/28/18.
+//  Copyright © 2018 Claudia Haddad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailsViewController : UIViewController
+
+@property (nonatomic, strong) NSDictionary *movie;
+
+@end
--- Flix/View Controllers/MoviesViewController.m
+++ Flix/View Controllers/MoviesViewController.m
@@ -8,16 +8,22 @@
 
 #import "MoviesViewController.h"
 #import "MovieCell.h"
+#import "DetailsViewController.h"
 #import "UIImageView+AFNetworking.h"
 
 @interface MoviesViewController () <UITableViewDataSource, UITableViewDelegate>
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (nonatomic, strong) NSArray *movies;
-
-
+@property (nonatomic,strong) UIRefreshControl *refreshControl;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
 @end
 
 @implementation MoviesViewController
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:YES];
+    [self.activityIndicator startAnimating];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -25,7 +31,16 @@
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
     
-    // Do any additional setup after loading the view.
+    [self fetchMovies];
+    
+    self.refreshControl = [[UIRefreshControl alloc] init];
+    [self.refreshControl addTarget:self action:@selector(fetchMovies)
+                  forControlEvents:UIControlEventValueChanged];
+    [self.tableView insertSubview:self.refreshControl atIndex:0];
+}
+
+
+- (void)fetchMovies {
     NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
     NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
@@ -44,10 +59,10 @@
             }
             
             [self.tableView reloadData];
-            // TODO: Get the array of movies
-            // TODO: Store the movies in a property to use elsewhere
-            // TODO: Reload your table view data
+            
+            [self.activityIndicator stopAnimating];
         }
+        [self.refreshControl endRefreshing];
     }];
     [task resume];
 }
@@ -69,7 +84,7 @@
     cell.titleLabel.text = movie[@"title"];
     cell.synopsisLabel.text = movie[@"overview"];
     
-    NSString *baseURLString = @"https://image.imdb.org/t/p/w500";
+    NSString *baseURLString = @"https://image.tmdb.org/t/p/w500";
     NSString *posterURLString = movie[@"poster_path"];
     NSString *fullPosterURLString = [baseURLString stringByAppendingString:posterURLString];
     NSURL *posterURL = [NSURL URLWithString:fullPosterURLString];
@@ -77,14 +92,20 @@
     return cell;
 }
 
-/*
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
+    UITableViewCell *tappedCell = sender;
+    NSIndexPath *indexPath = [self.tableView indexPathForCell:tappedCell];
+    NSDictionary *movie = self.movies[indexPath.row];
+    DetailsViewController *detailViewController = [segue destinationViewController];
+    detailViewController.movie = movie;
+    NSLog(@"Tapping on a movie!");
 }
-*/
+
 
 @end
