962f03efcb9f8ac4a6da81ee62ef7d6d4a1e6557
Claudia Haddad <cmhaddad@fb.com>
2018-06-27 21:17:45 -0700

    progress

---
+++ Flix/Views/MovieCell.h
@@ -0,0 +1,16 @@
+//
+//  MovieCell.h
+//  Flix
+//
+//  Created by Claudia Haddad on 6/27/18.
+//  Copyright © 2018 Claudia Haddad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MovieCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *posterView;
+@property (weak, nonatomic) IBOutlet UILabel *synopsisLabel;
+
+@end
